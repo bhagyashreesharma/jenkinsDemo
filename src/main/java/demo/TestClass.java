245aece84879590eca195d717cfcebeb1ac87653
bsharma <bhagyashreesharma90@gmail.com>
2018-06-21 12:09:31 +0530

    Modify code for sonar gate success

--- src/main/java/demo/TestClass.java
+++ src/main/java/demo/TestClass.java
@@ -1,16 +1,27 @@
 package demo;
 
 import java.util.logging.Logger;
-
+/*
+ * This is TestClass for jenkins demo
+ */
 public class TestClass {
 
 	static Logger logger = Logger.getLogger("TestClass");
 	private static final String NAME = "Bhagyashree";
+	
+	/**
+	 * This method is logging string
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		String str = getString();
 		logger.info(str);
 	}
 	
+	/**
+	 * This method returns String constant
+	 * @return String NAME
+	 */
 	public static String getString() {
 		return NAME;
 	}
--- src/test/java/demo/JunitTestClass.java
+++ src/test/java/demo/JunitTestClass.java
@@ -5,8 +5,16 @@ import org.junit.Test;
 
 import demo.TestClass;
 
+/**
+ * This is Junit Test class for TestClass
+ * @author bsharma
+ *
+ */
 public class JunitTestClass {
 
+/**
+ * This is Test method for getString class
+ */
 @Test
 public void test() {
 	Assert.assertEquals("Bhagyashree", TestClass.getString());
